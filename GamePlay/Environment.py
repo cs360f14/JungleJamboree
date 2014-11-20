056a40d83bb38c2e4849ba18c1edace0e5a0dc1b
Brianna Alcoran <alco8653@pacificu.edu>
2014-11-19 19:23:02 -0800

    added new files

---
+++ GamePlay/Environment.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+##################################
+# File Name: Environment.py
+# Author: 	 Group 3
+# Date: 	 11/10/2014
+# Class:	 CS360
+# Assignment:Jungle Jamboree
+# Purpose: 	 Environment class
+##################################
---
+++ GamePlay/RandomEvents.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+##################################
+# File Name: RandomEvents.py
+# Author: 	 Group 3
+# Date: 	 11/10/2014
+# Class:	 CS360
+# Assignment:Jungle Jamboree
+# Purpose: 	 RandomEvents class
+##################################
+
+
