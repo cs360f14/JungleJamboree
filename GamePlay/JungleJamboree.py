506fb073c1758ac812d4debd7dc6fc86e3acff0b
Evan Heydemann <heyd5159@pacificu.edu>
2014-11-19 18:57:10 -0800

    Added Jungle Jamboree File

---
+++ GamePlay/JungleJamboree.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+##################################
+# File Name: JungleJamboree.py
+# Author: 	 Group 3
+# Date: 	 11/10/2014
+# Class:	 CS360
+# Assignment:Jungle Jamboree
+# Purpose: 	 To run the game
+##################################
+
+"""
+The Main Module
+"""
+
+def main() :
+	""" Initializes a new game
+	"""
+	answer = 3 + 4 
+	print answer
+	print "Hi"
+	
+	
+	
+if __name__=="main" :
+	main ()
