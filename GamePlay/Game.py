f4db4883f877c77753cabc58d42f05047c398662
Evan Heydemann <heyd5159@pacificu.edu>
2014-12-03 15:54:15 -0800

    add a couple of images and cleaned up the Game.py

--- GamePlay/Game.py
+++ GamePlay/Game.py
@@ -11,6 +11,7 @@
 import pygame 
 from pygame.locals import *
 from Party import *
+from Store import *
 import sys
 
 """
@@ -25,10 +26,12 @@ class Game :
 		self._running = True
 		self._display = None
 		self._party = Party()
+		self._store = Store ()
+		self._store.setUpStore ()
 		#self._party.setUpParty () #don't forget to add this not in init
 		self._state = "Start"
-		self._width = 400
-		self._height = 300
+		self._width = 800
+		self._height = 600
 		self._size = (self._width, self._height)
 		pygame.init()
 		self._display = pygame.display.set_mode(self._size)
@@ -39,7 +42,7 @@ class Game :
 	
 	def on_loop (self):
 		self._display.blit(imgStart, (imgx,imgy))
-		self._display.blit(start, (150,145))
+		self._display.blit(start, (400,400))
 		while(True):
 			mouse = pygame.mouse.get_pos()
 			for event in pygame.event.get():
@@ -52,74 +55,105 @@ class Game :
 					sys.exit()
 					
 				if self._state == "Start":
-					if event.type == MOUSEBUTTONDOWN:
-						if mouse[0] > 150:
-							if mouse[0] < 197:
-								if mouse[1] > 145:
-									if mouse[1] < 157:
-										print("test success")
-										self._state = "Store"
-										
-				
-				
+					self.startButton (mouse, event)
+															
 				elif self._state == "Store":
-					self._display.blit(imgStore, (imgx, imgy))
-					self._display.blit(funds, (297, 279))
-					self._display.blit(leave1, (20, 279))
-					if event.type == MOUSEBUTTONDOWN:
-						if mouse[0] > 20:
-							if mouse[0] < 60:
-								if mouse[1] > 279:
-									if mouse[1] < 291:
-										self._state = "Home"
-					
-					
+					self.store (mouse, event)
 										
 				elif self._state == "Home":
-					self._display.blit(imgJungle, (imgx, imgy))
-					self._display.blit(imgPerson1, (imgxP1, imgyP1))
-					self._display.blit(imgPerson2, (imgxP2, imgyP2))
-					self._display.blit(imgPerson3, (imgxP3, imgyP3))
-					self._display.blit(imgPerson4, (imgxP4, imgyP4))
-					self._display.blit(imgPerson5, (imgxP5, imgyP5))
-					self._display.blit(store, (10, 225))
-					self._display.blit(forage, (10, 240))
-					self._display.blit(inventory, (10, 255))
-					self._display.blit(party, (10, 270))
-					self._display.blit(day, (170, 205))
-					self._display.blit(distance, (170, 225))
-					if event.type == MOUSEBUTTONDOWN:
-						if mouse[0] > 8:
-							if mouse[0] < 45:
-								if mouse[1] > 226:
-									if mouse[1] < 233:
-										self._state = "Store"
-										
-					if event.type == MOUSEBUTTONDOWN:
-						if mouse[0] > 8:
-							if mouse[0] < 69:
-								if mouse[1] > 255:
-									if mouse[1] < 265:
-										self._state = "Inventory"
-										
-										
+					self.home (mouse, event)
+									
 				elif self._state == "Inventory":
-					self._display.blit(imgInventory, (imgx, imgy))
-					self._display.blit(leave2, (20, 279))
-					if event.type == MOUSEBUTTONDOWN:
-						if mouse[0] > 20:
-							if mouse[0] < 60:
-								if mouse[1] > 279:
-									if mouse[1] < 291:
-										self._state = "Home"
-					
+					self.inventory (mouse, event)
+
 			#the update for the screen		
 			pygame.display.update()
+			
+	def startButton (self, mouse, event) :
+		if event.type == MOUSEBUTTONDOWN:
+			if self.checkMouse (mouse, 395, 460, 395, 415) :
+				self._state = "Store"
+							
+	def store (self, mouse, event) :
+		self._display.blit(imgStore, (imgx, imgy))
+		self._display.blit(funds, (297, 279))
+		self._display.blit(leave1, (20, 279))
+		self.displayInventory (self._store, mouse, event)
+		if event.type == MOUSEBUTTONDOWN:
+			if self.checkMouse (mouse, 20, 60, 279, 291) :
+				self._state = "Home"
+							
+	def home (self, mouse, event) :
+		self._display.blit(imgJungle, (imgx, imgy))
+		self._display.blit(imgPerson1, (imgxP1, imgyP1))
+		self._display.blit(imgPerson2, (imgxP2, imgyP2))
+		self._display.blit(imgPerson3, (imgxP3, imgyP3))
+		self._display.blit(imgPerson4, (imgxP4, imgyP4))
+		self._display.blit(imgPerson5, (imgxP5, imgyP5))
+		self._display.blit(store, (10, 225))
+		self._display.blit(forage, (10, 240))
+		self._display.blit(inventory, (10, 255))
+		self._display.blit(party, (10, 270))
+		self._display.blit(day, (170, 205))
+		self._display.blit(distance, (170, 225))
+		if event.type == MOUSEBUTTONDOWN:
+			if self.checkMouse (mouse, 8, 45, 226, 233) :
+				self._state = "Store"
+		
+		if event.type == MOUSEBUTTONDOWN:
+			if self.checkMouse (mouse, 8, 69, 255, 265) :
+				self._state = "Inventory"
+		
+	def inventory (self, mouse, event) :
+		self._display.blit(imgInventory, (imgx, imgy))
+		self._display.blit(leave2, (20, 279))
+		self.displayInventory (self._party.getInventory(), mouse, event)
+		if event.type == MOUSEBUTTONDOWN:
+			if self.checkMouse (mouse, 26, 60, 279, 291) :
+				self._state = "Home"
 
+	def checkMouse (self, mouse, left, right, top, bottom):
+		isIn = False
+		if mouse[0]	> left and mouse[0] < right \
+		and mouse[1] > top and mouse[1] < bottom :
+			isIn = True
+		return isIn
+		
+	def displayInventory (self, inventory, mouse, event) :
+		"""displays the inventory in a nice fashion"""
+		myFont = pygame.font.Font('freesansbold.ttf', 15) # figure out different way
+		
+		stringHeight = 40
+		cashString = "Cash:  $" + str(inventory.getCash())
+		cash = myFont.render(cashString, 1, (255,255,255))
+		self._display.blit(cash, (10, stringHeight))
+		stringHeight += 30
+		foodString = "Food:  " + str(inventory.getFood())
+		food = myFont.render(foodString, 1, (255,255,255))
+		self._display.blit(food, (10, 70))
+		stringHeight += 30
+
+		count = 0
+		for item in inventory.generateItem() :
+			itemTexts = myFont.render(str(item), 1, (255, 255, 255))	
+			self._display.blit(itemTexts, (10, stringHeight))								
+			stringHeight += 30
+			count += 1
+			
+	def getItemSelection (self, inventory, mouse, event) :
+		myFont = pygame.font.Font('freesansbold.ttf', 15)	
+		
+		stringHeight = 70
+		if event.type == MOUSEBOTTONDOWN:
+			if self.checkMouse (mouse, 5, 100, stringHeight - 5, \
+			stringHeight + 25) :
+				pass
+					
+									
 	
 #setDisplay = pygame.display.set_mode((400,300))
 pygame.display.set_caption('Jungle Jamboree')
-imgStart = pygame.image.load('Images/TestBackground.png')
+imgStart = pygame.image.load('Images/StartScreen.png')
 imgJungle = pygame.image.load('Images/JungleBackGround1.png')
 imgStore = pygame.image.load('Images/ShopBackground.png')
 imgInventory = pygame.image.load('Images/Backpack.png')
